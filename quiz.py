82e1be688da36330f2c013460d8ffaab23afdb7f
Jon Mockbee <mockbee1000@gmail.com>
2021-08-08 19:54:34 -0400

    working on the second question

--- quiz.py
+++ quiz.py
@@ -14,3 +14,17 @@ print("The distance in kilometers is " + str (my_trip_km))
 # 4) Calculate the round-trip in kilometers by doubling the result,
 #    and fill in the blank to print the result
 print("The round-trip in kilometers is " + str(my_trip_km*2))
+
+
+# This function compares two numbers and returns them
+# in increasing order.
+def order_numbers(number1, number2):
+	if number2 > number1:
+		return number1, number2
+	else:
+		return number2, number1
+
+# 1) Fill in the blanks so the print statement displays the result
+#    of the function call
+___, ___ = order_numbers(100, 99)
+print(smaller, bigger)
